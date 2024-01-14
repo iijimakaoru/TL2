6c4bda18bd31fd8e469d327c14f83338b9e89f6f
IijimaKaoru <k021g1465@g.neec.ac.jp>
2024-01-15 01:24:55 +0900

    02_03

--- TL2/main.cpp
+++ TL2/main.cpp
@@ -1,9 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main()
+int main(int argc, char* argv[])
 {
-	printf("Hello,World\n");
+	for (int i = 0; i < argc; i++)
+	{
+		// •¶Žš—ñargv‚Ìi”Ô‚ð•\Ž¦
+		printf(argv[i]);
+		// ‰üs
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
